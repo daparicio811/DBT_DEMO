a68c05dac6de4860c6dea1b440b571aba74e0869
Daniel Aparicio <dannyaparicio@gmail.com>
2025-10-27 15:48:34 +0000

    date utilities sql macros file

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,21 +4,12 @@ select TO_TIMESTAMP(STARTED_AT) AS STARTED_AT, /*"STARTED_AT" from BIKE Table in
 "AS STARTED_AT" renames header from TO_TIMESTAMP(STARTED_AT) to "STARTED_AS" */
 DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT, -- Parses out only DATE from timestamp. 
 HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT, -- Parse out Hour only
-CASE
-WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat','Sun') --Shows day of week name and filters out Sat and Sun
-THEN 'WEEKEND'
-ELSE 'BUSINESSDAY'
-END AS DAY_TYPE,
 
-CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
-    THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
-    THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
-    THEN 'SUMMER'
-    ELSE 'AUTUMN'
-    END AS STATION_OF_YEAR
+{{get_day_type('STARTED_AT')}} AS DAY_TYPE, --Logic is in macros folder data_utils.sql file, we are calling the macros here
 
+{{get_season('STARTED_AT')}} AS STATION_OF_YEAR --Logic is in macros folder data_utils.sql file, we are calling the macros here
+
+   
 
 from
 {{ source('DEMO', 'BIKE') }}
